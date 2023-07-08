16cf93b4438f7062817e60699de997606319cc3f
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-07-08 15:39:53 +0530

    routes

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const jsonServer = require("json-server"); // importing json-server library
-const server = jsonServer.create();
+const express = require("express");
+const jsonServer = require("json-server");
+
+const app = express();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080;
 
-server.use(middlewares);
-server.use(router);
+app.use(middlewares);
 
-server.listen(port);
+app.get("/cardDetails", (req, res) => {
+  const cardDetails = router.db.get("cardDetails");
+  res.json(cardDetails);
+});
+
+app.get("/camping", (req, res) => {
+  const camping = router.db.get("camping");
+  res.json(camping);
+});
+
+app.get("/city", (req, res) => {
+  const city = router.db.get("city");
+  res.json(city);
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
